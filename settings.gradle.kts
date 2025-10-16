1527f08fb29ac445b7945d73bf482b60951d4709
darkatek7 <heeltobias@gmail.com>
2025-10-16 09:14:54 +0200

    add cradle plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -27,6 +27,9 @@ pluginManagement {
         id("com.diffplug.spotless") version spotlessVersion
     }
 }
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+}
 
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
